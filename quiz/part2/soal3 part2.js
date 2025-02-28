054b79451f0450d5a3d85aa2e470addcc7481d99
YOHANES1212 <guidoboginbogin@gmail.com>
2025-03-01 00:23:06 +0700

    push soal 3 dan 4 part2

---
+++ quiz/part2/soal3 part2.js
@@ -0,0 +1,15 @@
+// Problem
+// Mirip seperti soal nomor 2, namun kali ini gunakan substring untuk mengambil potongan dari tiap kata!
+
+let word3 = "wow JavaScript is so cool";
+let exampleFirstWord1 = word3.substring(0, 3);
+let exampleDoubleWord2 = word3.substring(4, 14);
+let exampleThirdWord3 = word3.substring(15, 17);
+let exampleFourWord4 = word3.substring(18, 20);
+let exampleFiveWord5 = word3.substring(21, 25);
+
+console.log("First Word: " + exampleFirstWord1);
+console.log("Double word: " + exampleDoubleWord2);
+console.log("Third Word: " + exampleThirdWord3);
+console.log("Four Word: " + exampleFourWord4);
+console.log("Five Word: " + exampleFiveWord5);
