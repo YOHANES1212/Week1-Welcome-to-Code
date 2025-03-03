e2e18858eb0e0e3bd0f78729c2927de5e1a0149b
YOHANES1212 <guidoboginbogin@gmail.com>
2025-03-03 15:50:05 +0700

    push soal1 part4

---
+++ quiz/part4/soal1 part4.js
@@ -0,0 +1,21 @@
+```js
+// Tugas 1
+// Buatlah sebuah fungsi bernama shoutOut(), yang mengembalikan nilai berupa "Halo Function!", 
+// yang kemudian akan ditampilkan di console.
+
+//bikin fungsinya disini
+
+console.log(shoutOut());
+
+
+// ------------------------------------
+console.log('')
+// ------------------------------------
+```;
+function shoutOut() {
+  return "Halo Function";
+}
+shoutOut(); // ini yang simple
+
+let result = shoutOut();
+console.log(result); // ini kita masukan variable baru sama aja outpunya
