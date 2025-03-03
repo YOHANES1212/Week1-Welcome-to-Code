b144b3191091cc20b72bd363334dc69ec9044555
YOHANES1212 <guidoboginbogin@gmail.com>
2025-03-03 16:01:25 +0700

    push soal2 part4

---
+++ quiz/part4/soal2 part4.js
@@ -0,0 +1,29 @@
+```js
+// Buatlah sebuah fungsi bernama calculateMultiply(), yang mengembalikan nilai berupa hasil kali dari 
+// dua parameter yang dikirim.
+
+//bikin fungsinya disini
+
+let num1 = 1
+let num2 = 2
+
+let hasilPerkalian = calculateMultiply(num1,num2);
+console.log(hasilPerkalian);
+
+// ------------------------------------
+console.log('')
+// -----------------------------------
+```;
+let num1 = 1;
+let num2 = 2;
+function calculateMultiply() {
+  // ini menggunakan luar scope function
+  console.log(num1 + num2);
+}
+
+function calculateMultiply2(num1, num2) {
+  // ini menggunakan function lokal did dalam parameter
+  return num1 * num2;
+}
+let hasilPerkalian = calculateMultiply2(5, 2);
+console.log(hasilPerkalian);
